83313ee29109125e08b4927ce16961da09bc5856
SnowWolf725 <snowwolf725@gmail.com>
2025-08-27 15:54:20 +0800

    init commit twrp 14.1

--- twrp_waffle.mk
+++ twrp_waffle.mk
@@ -1,20 +1,7 @@
 #
-# Copyright 2017 The Android Open Source Project
+# Copyright (C) 2024 The Android Open Source Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-#
-# Only the below variable(s) need to be changed!
+# SPDX-License-Identifier: Apache-2.0
 #
 # Identifier for common folder
 COMMON_SOC := sm86xx
@@ -49,6 +36,17 @@ PRODUCT_SYSTEM_NAME := $(PRODUCT_MODEL)
 PRODUCT_SYSTEM_DEVICE := OP595DL1
 PRODUCT_BRAND := $(BOARD_VENDOR)
 PRODUCT_MANUFACTURER := $(PRODUCT_BRAND)
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := $(PRODUCT_RELEASE_NAME)
+
+# Theme
+TW_STATUS_ICONS_ALIGN := center
+TW_Y_OFFSET := 111
+TW_H_OFFSET := -111
+
+# SPR
+PRODUCT_PROPERTY_OVERRIDES += vendor.display.enable_spr=1
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
